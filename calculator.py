e09f2e240b7ad8332046567947697957b764e7ec
Phillip <phwitte@zedat.fu-berlin.de>
2021-10-22 15:01:22 +0200

    added operators and equals function

--- calculator.py
+++ calculator.py
@@ -12,7 +12,16 @@ def press(x):
 
     equation.set(expression)
 
+def equals():
+    try:
+        global expression
+        total = str(eval(expression))
+        equation.set(total)
 
+        expression = ""
+    except:
+        equation.set("Error")
+        expression = ""
 
 if __name__ == '__main__':
 # create a GUI window
@@ -25,7 +34,7 @@ if __name__ == '__main__':
     gui.title("Simple Calculator")
 
     # set the configuration of GUI window
-    gui.geometry("270x150")
+    gui.geometry("300x200")
 
     equation = StringVar()
 
@@ -40,8 +49,25 @@ if __name__ == '__main__':
         button.grid(column = (i-1)%3, row = math.floor((i-1)/3) + 1)
 
     button0 = Button(gui, text = str(0), command= lambda :press(0))
-
     button0.grid(column = 1, row = 4)
+
+    buttonPlus = Button(gui, text = "+", command= lambda :press("+"))
+    buttonPlus.grid(column = 3 , row = 1)
+
+    buttonMinus = Button(gui, text = "-", command= lambda :press("-"))
+    buttonMinus.grid(column = 3 , row = 2)
+
+    buttonMult = Button(gui, text = "*", command= lambda :press("*"))
+    buttonMult.grid(column = 3 , row = 3)
+
+    buttonDiv = Button(gui, text = "/", command= lambda :press("/"))
+    buttonDiv.grid(column = 3 , row = 4, columnspan=3)
+
+    buttonPoint = Button(gui, text = ".", command= lambda :press("."))
+    buttonPoint.grid(column = 3 , row = 5, columnspan=3)
+
+    buttonEqu = Button(gui, text = "=", command= lambda :equals())
+    buttonEqu.grid(column = 0 , row = 5, columnspan = 3, ipadx=70)
 
      # start the GUI
     gui.mainloop()
